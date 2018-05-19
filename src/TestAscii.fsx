5362ec51320a400d72308b7dae5cfad5a55adc4e
stephentetley <stephen.tetley@gmail.com>
2018-05-19 10:56:33 +0100

    Added module to generate Ascii art histograms.

---
+++ src/TestAscii.fsx
@@ -0,0 +1,10 @@
+﻿
+#load @"WaterData\AsciiArt.fs"
+open WaterData.AsciiArt
+
+let test01 () = 
+    pick 0.0
+
+let test02 () = 
+    let conv (i:int) : double = double i / 10.0
+    asciiHisto conv (seq {0..10})
---
+++ src/WaterData/AsciiArt.fs
@@ -0,0 +1,18 @@
+﻿module WaterData.AsciiArt
+
+let palette : char [] = 
+    " .,^:*#$X@".ToCharArray ()
+
+let inline clamp (d:double) : double = 
+    match d with
+    | d when d > 0.9 -> 0.9 
+    | d when d < 0.0 -> 0.0
+    | d -> d
+
+let pick (d:double) : char = 
+    let ix = int (clamp d * 10.0) 
+    palette.[ix]
+
+let asciiHisto (normalize:'v -> double) (source:seq<'v>) : string = 
+    Seq.map (pick << normalize) source |> Seq.toArray |> System.String
+    
